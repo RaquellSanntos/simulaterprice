d9db3d30ef21af74c66cd9cef9d9dbd0a7855357
Raquel Noro <santos.soares0424@gmail.com>
2024-11-29 17:50:40 -0300

    iniciando projeto piloto Simulador de Custos

---
+++ src/main/java/com/noroaco/simulaterprice/SimulaterpriceApplication.java
@@ -0,0 +1,13 @@
+package com.noroaco.simulaterprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulaterpriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulaterpriceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noroaco/simulaterprice/SimulaterpriceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.noroaco.simulaterprice;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulaterpriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
